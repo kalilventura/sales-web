7f3dbde7eadecf8d6dab759dc7380380d65dddd7
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-04-14 23:39:27 -0300

    Criacao do formulario

--- Frontend/app/src/app/Interface/department/create/create-department.component.ts
+++ Frontend/app/src/app/Interface/department/create/create-department.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create-department',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-department.component.css']
 })
 export class CreateDepartmentComponent implements OnInit {
-
-  constructor() { }
+  createDepartmentForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm() {
+    this.createDepartmentForm = this.fb.group({
+      name: ['', [Validators.required]]
+    });
+  }
+
+
+  saveDepartment() {
+    console.log(this.createDepartmentForm.value);
+  }
 }
--- Frontend/app/src/app/Interface/department/create/create-department.module.ts
+++ Frontend/app/src/app/Interface/department/create/create-department.module.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CreateDepartmentComponent } from './create-department.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     CreateDepartmentComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class CreateDepartmentModule { }
