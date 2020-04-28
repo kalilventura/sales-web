e852594c72c44237f58ce79ce80e9171132e660e
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-04-27 22:37:54 -0300

    Modificacao da organização das pastas do projeto

--- Frontend/app/src/app/app.module.ts
+++ Frontend/app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuModule } from './components/menu/menu.module';
-import { FooterComponent } from './components/footer/footer.component';
+import { MenuModule } from './components/template/menu/menu.module';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
